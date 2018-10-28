341bafe432a33089909c824d2f34aefd46446853
VIVAFROG <vivafrog@gmail.com>
2018-10-28 23:14:29 +0900

    4-1

--- App.js
+++ App.js
@@ -2,25 +2,41 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Weather from "./Weather";
 
+const API_KEY = "3c3dad32a746233eb9f6ac6906a311d4";
+
 export default class App extends React.Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature:null,
+    name:null
   };
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState({
-          error:error
-        })
+          error: error
+        });
       }
     );
   }
+
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+      this.setState({
+        temperature:json.main.temp,
+        name:json.weather[0].main,
+        isLoaded: true
+      })
+      });
+  };
+
   render() {
     const { isLoaded, error } = this.state;
     return (
@@ -44,9 +60,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  errorText:{
-    color:"red",
-    backgroundColor:"transparent",
+  errorText: {
+    color: "red",
+    backgroundColor: "transparent",
     marginBottom: 40
   },
   loading: {
